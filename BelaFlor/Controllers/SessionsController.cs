927219fe59d179b81a017239f2ebf2a2e9815a5d
Joao Ladeiras <jtladeiras@gmail.com>
2013-11-22 00:29:30 +0000

    Fresh copy

---
+++ BelaFlor/Controllers/SessionsController.cs
@@ -0,0 +1,56 @@
+﻿using BelaFlor.Lib_Primavera;
+using BelaFlor.Lib_Primavera.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FlowerPow.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        // GET api/default1/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/default1
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /*public HttpResponseMessage PostRegisto(Utilizador utilizador)
+        {
+            RespostaErro erro = new RespostaErro();
+            erro = Comercial.RegistaUtilizador(utilizador);
+
+            if (erro.Erro == 0)
+            {
+                var response = Request.CreateResponse(
+                   HttpStatusCode.Created, utilizador);
+                string uri = Url.Link("ActionApi", new { CodCliente = utilizador.CodCliente });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+        }*/
+
+        // PUT api/default1/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/default1/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BelaFlor/Lib_Primavera/Model/LinhaDocVenda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BelaFlor.Lib_Primavera.Model
+{
+    public class LinhaDocVenda
+    {
+        public dynamic IdCabecDoc { get; set; }
+
+        public dynamic DescArtigo { get; set; }
+
+        public dynamic Unidade { get; set; }
+
+        public double TotalILiquido { get; set; }
+
+        public double TotalLiquido { get; set; }
+
+        public string CodArtigo { get; set; }
+
+        public double Quantidade { get; set; }
+
+        public double Desconto { get; set; }
+
+        public double PrecoUnitario { get; set; }
+    }
+}
